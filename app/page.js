805709c89df4b6f87c0f76647a2ef9cfad507ec7
vaswinraaju-png <v.aswinraaju@gmail.com>
2025-12-26 12:48:15 +0530

    page.js

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "20px" }}>
+      <h1>Clear Skin Essentials</h1>
+      <p>Honest skincare education. Ingredient-first. No selling.</p>
+      <a href="/blog">Read Blogs</a>
+    </main>
+  );
+}
